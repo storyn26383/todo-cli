f950726304b6a18bf31edaf03c02fad39c3f8867
Sasaya <storyn26383@gmail.com>
2023-10-16 23:07:04 +0800

    chore: apply coding style

--- app/helpers.php
+++ app/helpers.php
@@ -2,5 +2,5 @@
 
 function home_storage_path(string $path = ''): string
 {
-    return $_SERVER['HOME'] . '/.' . app('name') . '/' . trim($path, '/');
+    return $_SERVER['HOME'].'/.'.app('name').'/'.trim($path, '/');
 }
